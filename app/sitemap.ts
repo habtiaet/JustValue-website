28ff578877b56aa4a3cd1be087c319e3cedea516
habtiaet <ljescheepers@ziggo.nl>
2025-11-06 11:41:59 +0100

    Update sitemap to use www.justvalue.nl domain

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://justvalue.nl'
+  const baseUrl = 'https://www.justvalue.nl'
   
   return [
     {
